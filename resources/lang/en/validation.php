42c2e1fae70673b55e5b033ea3f8ff520a66a471
Maxime Pelletier <pelletiermaxime@gmail.com>
2018-07-29 19:14:15 -0400

    Strict types!

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,7 +1,8 @@
 <?php
 
-return [
+declare(strict_types=1);
 
+return [
     /*
     |--------------------------------------------------------------------------
     | Validation Language Lines
@@ -115,5 +116,4 @@ return [
     */
 
     'attributes' => [],
-
 ];
